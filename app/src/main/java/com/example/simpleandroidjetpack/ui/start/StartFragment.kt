11eee2aeab9ce3ab4cee60e355a9387e22adfb36
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2020-08-24 20:48:44 -0300

    creating simple fragment navigation

--- app/src/main/java/com/example/simpleandroidjetpack/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/simpleandroidjetpack/ui/start/StartFragment.kt
@@ -1,13 +1,17 @@
 package com.example.simpleandroidjetpack.ui.start
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.simpleandroidjetpack.R
+import kotlinx.android.synthetic.main.fragment_start.*
 
 class StartFragment : Fragment() {
+    private lateinit var listener: OnButtonClicked
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,9 +19,29 @@ class StartFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_start, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnNext.setOnClickListener {
+            listener.buttonClicked()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is OnButtonClicked) {
+            listener = context
+        }
+    }
+
     companion object {
         fun newInstance(): StartFragment {
             return StartFragment()
         }
+    }
+
+    interface OnButtonClicked {
+        fun buttonClicked()
     }
 }
