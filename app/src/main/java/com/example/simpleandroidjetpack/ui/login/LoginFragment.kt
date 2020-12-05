ba6c03bd18ed98552630a0e18ed34d43f53fb314
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2020-12-05 18:40:24 -0300

    chore(LoginFragment): implemented state control with Sealed Class

--- app/src/main/java/com/example/simpleandroidjetpack/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/simpleandroidjetpack/ui/login/LoginFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.simpleandroidjetpack.R
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.login_fragment.*
 
 class LoginFragment : Fragment() {
 
@@ -19,9 +22,33 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.login_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        viewModel.authenticationStateEvent.observe(viewLifecycleOwner, { authenticationState ->
+            when (authenticationState) {
+                is LoginViewModel.AuthenticationState.InvalidAuthentication -> {
+                    val validationFields: Map<String, TextInputLayout> = initValidationFields()
+
+                    authenticationState.fields.forEach { fieldError ->
+                        validationFields[fieldError.first]?.error = getString(fieldError.second)
+                    }
+                }
+            }
+        })
+
+        buttonLoginSignIn.setOnClickListener {
+            val username = inputLoginUsername.text.toString()
+            val password = inputLoginPassword.text.toString()
+
+            viewModel.authentication(username, password)
+        }
     }
 
+    private fun initValidationFields() = mapOf(
+        LoginViewModel.INPUT_USERNAME.first to inputLayoutLoginUsername,
+        LoginViewModel.INPUT_PASSWORD.first to inputLayoutLoginPassword
+    )
 }
