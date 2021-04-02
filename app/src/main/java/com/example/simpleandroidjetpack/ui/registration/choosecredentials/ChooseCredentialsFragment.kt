2a5d2f24100b722bdb7f605633ea8f167f7cc898
Matheus H. de Souza <mh.matheussouza@gmail.com>
2021-04-01 22:00:13 -0300

    feat(choose credentials): implemented choose credentials

--- app/src/main/java/com/example/simpleandroidjetpack/ui/registration/choosecredentials/ChooseCredentialsFragment.kt
+++ app/src/main/java/com/example/simpleandroidjetpack/ui/registration/choosecredentials/ChooseCredentialsFragment.kt
@@ -3,15 +3,30 @@ package com.example.simpleandroidjetpack.ui.registration.choosecredentials
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.simpleandroidjetpack.R
+import com.example.simpleandroidjetpack.ui.login.LoginViewModel
+import com.example.simpleandroidjetpack.ui.registration.RegistrationViewModel
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.fragment_choose_credentials.*
 
 class ChooseCredentialsFragment : Fragment() {
 
+    private val loginViewModel: LoginViewModel by activityViewModels()
+    private val registrationViewModel: RegistrationViewModel by activityViewModels()
+
     private val args: ChooseCredentialsFragmentArgs by navArgs()
+
+    private val navController: NavController by lazy {
+        findNavController()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +38,62 @@ class ChooseCredentialsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
 
         textChooseCredentialsName.text = getString(R.string.choose_credentials_text_name, args.name)
+
+        val invalidFields = initValidationFields()
+        listenToRegistrationStateEvent(invalidFields)
+        registerViewsListeners()
+        registerDeviceBackStack()
+    }
+
+    private fun initValidationFields() = mapOf(
+        RegistrationViewModel.INPUT_USERNAME.first to inputLayoutChooseCredentialsUsername,
+        RegistrationViewModel.INPUT_PASSWORD.first to inputLayoutChooseCredentialsPassword,
+    )
+
+    private fun listenToRegistrationStateEvent(validationFields: Map<String, TextInputLayout>) {
+        registrationViewModel.registrationStateEvent.observe(viewLifecycleOwner, { registrationState ->
+            when (registrationState) {
+                is RegistrationViewModel.RegistrationState.RegistrationCompleted -> {
+                    val token = registrationViewModel.authToken
+                    val username = inputChooseCredentialsUsername.text.toString()
+
+                    loginViewModel.authenticateToken(token, username)
+                    navController.popBackStack(R.id.profileFragment, false)
+                }
+                is RegistrationViewModel.RegistrationState.InvalidCredentials -> {
+                    registrationState.fields.forEach { fieldError ->
+                        validationFields[fieldError.first]?.error = getString(fieldError.second)
+                    }
+                }
+            }
+        })
+    }
+
+    private fun registerViewsListeners() {
+        buttonChooseCredentialsNext.setOnClickListener {
+            val username = inputChooseCredentialsUsername.text.toString()
+            val password = inputChooseCredentialsPassword.text.toString()
+
+            registrationViewModel.createCredentials(username, password)
+        }
+    }
+
+    private fun registerDeviceBackStack() {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            cancelRegistration()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        cancelRegistration()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun cancelRegistration() {
+        registrationViewModel.userCancelledRegistration()
+        navController.popBackStack(R.id.loginFragment, false)
     }
 }
